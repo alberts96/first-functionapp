2e216331b8db48c3e24008bcccfcdf03dd5d13bb
alberts96 <carloalbertocarrucciu@outlook.com>
2021-12-23 11:29:49 +0100

    deploy-prova

--- TimerTrigger1/__init__.py
+++ TimerTrigger1/__init__.py
@@ -8,6 +8,7 @@ def main(mytimer: func.TimerRequest) -> None:
     logging.warning("hello world")
      #######################################
     print("hello world")
+    logging.warning("prova-deploy")
  
     logging.info('Python timer trigger function ran at %s', utc_timestamp)
     return None
